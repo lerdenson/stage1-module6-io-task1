cac420f0e69f0fa5497e9f9df2866292908932b7
Максим <Maxim.d.morozov@gmail.com>
2023-07-11 21:00:06 +0400

    upd

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,42 @@
 package com.epam.mjc.io;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.HashMap;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        StringBuilder data = new StringBuilder();
+        try (FileInputStream fr = new FileInputStream(file)) {
+            int c;
+            while ((c = fr.read()) != -1) {
+                data.append((char) c);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        HashMap<String, String> parsedData = new HashMap<>();
+
+        String[] lines = data.toString().split("\n");
+        for (String line : lines) {
+            String[] parts = line.split(": ", 2);
+            parsedData.put(parts[0].trim(), parts[1].trim());
+        }
+
+        Profile profile = new Profile(
+                parsedData.get("Name"),
+                Integer.parseInt(parsedData.get("Age")),
+                parsedData.get("Email"),
+                Long.parseLong(parsedData.get("Phone"))
+        );
+
+        System.out.println(profile);
+
+        return profile;
+
     }
 }
